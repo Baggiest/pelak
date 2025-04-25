35dbddce54a3d477c6f638261394e1871fe524f7
baggiest <mani.ehsanisohi@gmail.com>
2025-04-26 03:00:32 +0330

    unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
 import { redirect } from 'next/navigation';
-import Image from 'next/image'
-import Link from 'next/link'
-import PelakLogo from '../../public/pelak.svg'
-import PelakIran from '../../public/pelak-iran.svg'
 
 export default function Home() {
   redirect('/find-city');
